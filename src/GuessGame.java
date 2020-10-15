e3b4cdda31f7f7fba37499499b3b792c456a69c8
Pavel _ <fpavel93@gmail.com>
2020-10-15 17:34:03 +0300

    Initial commit

---
+++ src/GuessGame.java
@@ -0,0 +1,46 @@
+
+import javax.swing.*;
+import java.util.Random;
+
+public class GuessGame {
+    int bestScore;
+
+    public void play(int max){
+
+        Random rand = new Random();
+        int randNum = rand.nextInt(max);
+        int count = 1;
+
+        String answer = JOptionPane.showInputDialog("Pleas guess a number between 0 and " + max);
+        int result = Integer.parseInt(answer);
+        while (result != randNum){
+            if(result < randNum){
+                answer = JOptionPane.showInputDialog("Larger");
+            }
+            else {
+                answer = JOptionPane.showInputDialog("Smaller");
+            }
+            result = Integer.parseInt(answer);
+            count++;
+        }
+        int score = max/count;
+        if(score > bestScore){
+            bestScore = score;
+        }
+    }
+
+    public void printBestScore() {
+        System.out.println("The best score is: " + bestScore);
+    }
+
+    public static void main(String[] args) {
+
+        int max = 100;
+
+        GuessGame game = new GuessGame();
+        for (int i = 0; i < 3; i++) {
+            game.play(max);
+        }
+        game.printBestScore();
+    }
+}
